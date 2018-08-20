f9b97ccca45ac6482971c248ecdd237c68d33d77
MiltonIp <35160207+MiltonIp@users.noreply.github.com>
2018-08-20 13:41:26 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr
+import webbrowser
+
+if __name__ == "__main__":
+    # Waiting for you to say "This is so sad, Alexa play Despacito"
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say This is so sad. Alexa play Despacito")
+        audio = r.listen(source)
+
+    try:
+        speech = r.recognize_sphinx(audio)
+        if "sad" in speech and ("alexa" in speech or "Alexa" in speech) and "play" in speech:
+           webbrowser.open("https://soundcloud.com/luisfonsiofficial/despacito", new=2)
+    except sr.UnknownValueError:
+        print("You did not say This is so sad, Alexa play Despacito")
+    except sr.RequestError as e:
+        print("Despacito error; {0}".format(e))
